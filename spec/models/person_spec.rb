c1e24d563dc943fed6af248d1c75e74239f24f86
lujanfernaud <lujanfernaud@gmail.com>
2017-12-07 09:43:26 +0000

    Modify specs descriptions

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,8 +1,8 @@
 require "spec_helper"
 
 describe Person do
-  describe "requires a name to be valid" do
-    it "is valid with a name" do
+  describe "validations" do
+    it "is valid with a first name" do
       person = described_class.new(first_name: "Dong")
 
       person.save
@@ -10,16 +10,16 @@ describe Person do
       expect(person).to be_valid
     end
 
-    it "is not valid without a name" do
-      person = described_class.new(first_name: nil)
+    it "is not valid with a blank first name" do
+      person = described_class.new(first_name: "")
 
       person.save
 
       expect(person).not_to be_valid
     end
 
-    it "is not valid with an empty string" do
-      person = described_class.new(first_name: "")
+    it "is not valid without a first name" do
+      person = described_class.new(first_name: nil)
 
       person.save
 

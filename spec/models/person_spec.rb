bad205a2af51e2c2944b1201c359604fe05db9df
lujanfernaud <lujanfernaud@gmail.com>
2017-12-07 09:37:35 +0000

    Implement validation specs for Person

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,9 +1,29 @@
 require "spec_helper"
 
 describe Person do
-  it "requires a name to be valid" do
-    # Write your spec here.
-    # You can delete the pending line.
-    pending "Not written yet"
+  describe "requires a name to be valid" do
+    it "is valid with a name" do
+      person = described_class.new(first_name: "Dong")
+
+      person.save
+
+      expect(person).to be_valid
+    end
+
+    it "is not valid without a name" do
+      person = described_class.new(first_name: nil)
+
+      person.save
+
+      expect(person).not_to be_valid
+    end
+
+    it "is not valid with an empty string" do
+      person = described_class.new(first_name: "")
+
+      person.save
+
+      expect(person).not_to be_valid
+    end
   end
 end
